7ba3e22f72a147838c61c8fbc017d2e0f2cb7335
mahesh perera <maheshmadusankap@gmail.com>
2023-06-02 11:26:08 +0200

    corsfix4

--- server/index.js
+++ server/index.js
@@ -39,8 +39,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://chatmev3.onrender.com"); 
- res.header("Access-Control-Allow-Credentials ", " true");
- res.header("Access-Control-Allow-Methods" , " GET, POST, OPTIONS");
+
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
