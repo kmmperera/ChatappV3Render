bd070decff5f3c10dfe60eae375a78b6e1e8dd51
mahesh perera <maheshmadusankap@gmail.com>
2023-06-02 10:39:57 +0200

    corsfix2

--- server/index.js
+++ server/index.js
@@ -37,6 +37,15 @@ mongoose
 app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+
 if(process.env.NODE_ENV === 'production'){
 		app.use(express.static('client/build'));
 	//app.use(express.static(path.join(__dirname, 'client/build')));
